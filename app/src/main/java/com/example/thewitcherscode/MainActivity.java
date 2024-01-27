b8202ae5407e20f86d6fcc4ed141cb0d717a0e03
Mederic15 <mede.hockey@gmail.com>
2024-01-27 17:03:29 -0500

    Ajout des icone profil, carte, louer dans la barre de navigation

--- app/src/main/java/com/example/thewitcherscode/MainActivity.java
+++ app/src/main/java/com/example/thewitcherscode/MainActivity.java
@@ -1,50 +1,59 @@
 package com.example.thewitcherscode;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import androidx.appcompat.widget.Toolbar;
-
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Toolbar tb_navigation;
+    BottomNavigationView bnv_navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tb_navigation = findViewById(R.id.tb_navigation);
-        setSupportActionBar(tb_navigation);
-    }
+        bnv_navigation = findViewById(R.id.bnv_navigation);
+        bnv_navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+                if (item.getItemId() == R.id.action_louer) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
+                } else if (item.getItemId() == R.id.action_marche) {
 
-        // Ajoutez des actions en fonction de l'item sélectionné
-        if (id == R.id.action_louer) {
-            // Faites quelque chose quand l'item "Settings" est sélectionné
-            return true;
-        } else if (id == R.id.action_marche) {
+                } else if (item.getItemId() == R.id.action_carte) {
 
-        } else if (id == R.id.action_carte) {
+                } else if (item.getItemId() == R.id.action_reseau) {
 
-        } else if (id == R.id.action_reseau) {
+                } else if (item.getItemId() == R.id.action_profil) {
 
-        } else if (id == R.id.action_profil) {
+                }
 
-        }
-
-        return super.onOptionsItemSelected(item);
+                /*switch (item.getItemId()) {
+                    case R.id.action_louer:
+                        // Action lorsque Item 1 est sélectionné
+                        return true;
+                    case R.id.action_marche:
+                        // Action lorsque Item 2 est sélectionné
+                        return true;
+                    case R.id.action_carte:
+                        // Action lorsque Item 3 est sélectionné
+                        return true;
+                    case R.id.action_reseau:
+                        // Action lorsque Item 4 est sélectionné
+                        return true;
+                    case R.id.action_profil:
+                        // Action lorsque Item 5 est sélectionné
+                        return true;
+                    default:
+                        return false;
+                }*/
+                return false;
+            }
+        });
     }
 }
