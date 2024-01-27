e74292f3a4651dd6179082c07e2a2911d0e0002b
BrouillardLiam <liambrouillard@outlook.fr>
2024-01-27 16:24:45 -0500

    shit load de modif viewPager fonctionnel

--- app/src/main/java/com/example/thewitcherscode/LouerActivity.java
+++ app/src/main/java/com/example/thewitcherscode/LouerActivity.java
@@ -1,8 +1,17 @@
 package com.example.thewitcherscode;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LouerActivity extends AppCompatActivity {
 
@@ -10,5 +19,30 @@ public class LouerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_louer);
+
+        ViewPager   VP_imageParcelle    =   findViewById(R.id.VP_imageParcelle);
+        Toolbar     tb_retour           =   findViewById(R.id.tb_retour);
+        TextView    tv_adresse          =   findViewById(R.id.tv_adresse);
+        setSupportActionBar(tb_retour);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            Parcelle parcelle = intent.getParcelableExtra("parcelle");
+
+            if (parcelle != null) {
+                List<Integer> images = parcelle.getImages();
+
+                if (images != null && !images.isEmpty()) {
+                    ImagePagerAdapter adapter = new ImagePagerAdapter(this, getSupportFragmentManager(), images);
+                    VP_imageParcelle.setAdapter(adapter);
+                } else {
+                    // Log ou gestion d'erreur si la liste d'images est null ou vide
+                }
+            }
+
+
+        }
     }
 }
--- app/src/main/java/com/example/thewitcherscode/MainActivity.java
+++ app/src/main/java/com/example/thewitcherscode/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.thewitcherscode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ouvrirLouerActivity(View view) {
+        Parcelle parcelleTest = createParcelleTest();
+        Intent intent = new Intent(this, LouerActivity.class);
+        intent.putExtra("parcelle", parcelleTest);
+        startActivity(intent);
+    }
+
+    private Parcelle createParcelleTest() {
+        List<Integer> images = new ArrayList<>();
+        images.add(R.drawable.terre_cultive);
+        images.add(R.drawable.terre_agricole);
+
+        Log.w("myApp",images+"");
+
+        return new Parcelle(
+                "Adresse de test",
+                500.0,
+                "Description de test",
+                "test@email.com",
+                "123456789",
+                5,
+                150.0,
+                images
+        );
     }
 }
