b0abd8fc774ab9ac6fe2f284bb0dbde9a1b66a48
Samuel Ferragne <xavier.racine2018@gmail.com>
2024-01-27 22:10:14 -0500

    Map on ETS missing import

--- app/src/main/java/com/example/thewitcherscode/MainActivity.java
+++ app/src/main/java/com/example/thewitcherscode/MainActivity.java
@@ -10,5 +10,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the SupportMapFragment and request notification when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+
+        LatLng defaultPos = new LatLng(45.49502662776999, -73.56203619520896);
+        CameraPosition camPos = new CameraPosition.Builder().target(defaultPos).zoom(15).build();
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(camPos));
+    }
+
 }
