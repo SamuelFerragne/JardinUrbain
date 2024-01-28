c419ab6f552add9615fc97fdf160ada9509283a1
Samuel Ferragne <xavier.racine2018@gmail.com>
2024-01-27 23:09:37 -0500

    IMplements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,10 @@ android {
     namespace = "com.example.thewitcherscode"
     compileSdk = 34
 
+    buildFeatures {
+        buildConfig = true
+    }
+
     defaultConfig {
         applicationId = "com.example.thewitcherscode"
         minSdk = 24
@@ -29,13 +33,17 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
 }
+
+
 
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
